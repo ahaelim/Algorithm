5856e9477b6922454502c58d33dd63dd770c7485
ahaelim <limahae1015@gmail.com>
2023-01-08 18:04:25 +0900

    중복된 숫자 개수

---
+++ src/lv0/중복된_수_개수.java
@@ -0,0 +1,19 @@
+package lv0;
+
+public class 중복된_수_개수 {
+    public static void main(String[] args) {
+//        정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때,
+//        array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성
+    }
+    class Solution {
+        public int solution(int[] array, int n) {
+            int answer = 0;
+            for(int i=0; i<array.length; i++){
+                if(array[i]==n){
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
